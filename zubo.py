a78213eb778a4738dfd4a691c03de661ed03a3c1
cx-bot100 <3206868632@qq.com>
2024-10-20 12:09:29 +0800

    Update zubo.py

--- zubo.py
+++ zubo.py
@@ -11,7 +11,7 @@ subnet_end = "14.152.79.255"
 ports = [6600, 5050, 5005, 8086, 8088, 8888, 9080, 8000, 9026, 3952, 9000, 9999, 5555, 4022, 7000]
 
 # 定义成功IP的文件
-successful_ips_file = "successful_zb.txt"
+successful_ips_file = "ips.txt"
 
 # 定义最大线程数
 max_threads = 10
