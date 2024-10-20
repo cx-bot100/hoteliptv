3659fc524752b5216ea78c15dd3ea77207abe14d
cx-bot100 <3206868632@qq.com>
2024-10-20 14:30:55 +0800

    Update zubo.py

--- zubo.py
+++ zubo.py
@@ -18,10 +18,10 @@ max_threads = 200
 
 # 检查IP和端口的连通性
 def check_ip_port(ip, port):
-    url = f"http://{ip}:{port}/udp/239.77.0.251:5146"
+    url = f"http://{ip}:{port}"
     try:
         response = urllib.request.urlopen(url, timeout=0.3)
-        if response.getcode() == 200:
+        if response.getcode() == 400:
             server_header = response.headers.get('Server')
             if server_header and 'udpxy' in server_header:
                 print(f"访问 {ip}:{port} 成功，Server: udpxy")
