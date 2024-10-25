f7faa9c91f18154a0386c7212cad4cf238f1d9d5
cx-bot100 <3206868632@qq.com>
2024-10-25 23:23:53 +0800

    Update itv.py

--- itv.py
+++ itv.py
@@ -5,7 +5,7 @@ import re
 # 目标URL列表
 urls = [
     "http://tonkiang.us/?iqtv=%E5%A4%A7%E6%B9%BE%E5%8C%BA%E5%8D%AB%E8%A7%86",
-    "http://tonkiang.us/?iqtv=%E5%A4%A7%E6%B9%BE%E5%8C%BA%E5%8D%AB%E8%A7%86%E9%AB%98%E6%B8%85"
+    "http://www.foodieguide.com/iptvsearch/?s=%E7%BF%A1%E7%BF%A0%E5%8F%B0&l=eowuxJvaa8bre/oee/oOWPsA=="
 ]
 
 # 保存结果的列表
@@ -22,7 +22,7 @@ for url in urls:
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # 查找所有class为'resultplus'的div元素
-        divs = soup.find_all('div', class_='resultplus')
+        divs = soup.find_all('div', class_='gapeot')
 
         # 提取包含http和https的IP地址
         for div in divs:
@@ -35,6 +35,6 @@ for url in urls:
 # 将结果写入key.txt文件，格式为“大湾区卫视,ip地址”
 with open('key.txt', 'w') as file:
     for ip in ip_addresses:
-        file.write(f"大湾区卫视,{ip}\n")
+        file.write(f"翡翠台,{ip}\n")
 
 print(f"已提取 {len(ip_addresses)} 个IP地址，并保存到key.txt文件中。")
