2d87701c50a65641b4fb768b892f38d32d036c46
cx-bot100 <3206868632@qq.com>
2024-10-26 14:41:45 +0800

    Update itv.py

--- itv.py
+++ itv.py
@@ -4,14 +4,20 @@ import re
 
 # 目标URL列表
 urls = [
-    "http://tonkiang.us/?iqtv=%E5%A4%A7%E6%B9%BE%E5%8C%BA%E5%8D%AB%E8%A7%86%E9%AB%98%E6%B8%85",
-    "http://tonkiang.us/?iqtv=%E5%B9%BF%E4%B8%9C%E7%8F%A0%E6%B1%9F%E9%AB%98%E6%B8%85"
+    "http://tonkiang.us/?iqtv=%E5%A4%A7%E6%B9%BE%E5%8C%BA%E5%8D%AB%E8%A7%86%E9%AB%98%E6%B8%85", #dwq
+    "http://tonkiang.us/?iqtv=%E5%B9%BF%E4%B8%9C%E7%8F%A0%E6%B1%9F%E9%AB%98%E6%B8%85", #gdzj
+    "http://tonkiang.us/?iqtv=%E5%B9%BF%E4%B8%9C%E4%BD%93%E8%82%B2%E9%AB%98%E6%B8%85", #gdty
+    "http://tonkiang.us/?iqtv=%E5%B9%BF%E4%B8%9C%E6%96%B0%E9%97%BB%E9%AB%98%E6%B8%85", #gdxw
+    "http://tonkiang.us/?iqtv=%E5%B9%BF%E4%B8%9C%E5%BD%B1%E8%A7%86%E9%AB%98%E6%B8%85" #gdys
 ]
 
 # 保存结果的字典
 ip_addresses = {
     "大湾区卫视": [],
-    "广东珠江高清": []
+    "广东珠江高清": [],
+    "广东体育高清": [],
+    "广东新闻高清": [],
+    "广东影视高清": [],
 }
 
 # 遍历每个URL
@@ -42,12 +48,19 @@ for url in urls:
                     ip_addresses["大湾区卫视"].extend(matches)
                 elif "广东珠江高清" in resultplus_name:
                     ip_addresses["广东珠江高清"].extend(matches)
+                elif "广东体育高清" in resultplus_name:
+                    ip_addresses["广东体育高清"].extend(matches)
+                elif "广东新闻高清" in resultplus_name:
+                    ip_addresses["广东新闻高清"].extend(matches)
+                elif "广东影视高清" in resultplus_name:
+                    ip_addresses["广东影视高清"].extend(matches)
     
     except Exception as e:
         print(f"Failed to scrape {url} due to {e}")
 
 # 将结果写入key.txt文件
 with open('key.txt', 'w') as file:
+    file.write('地方频道,#genre#\n')
     for resultplus, ips in ip_addresses.items():
         for ip in ips:
             file.write(f"{resultplus},{ip}\n")
