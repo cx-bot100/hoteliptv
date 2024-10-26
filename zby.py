be846972176fc9c8a4f2cccf6eb666fd5dc398a3
cx-bot100 <3206868632@qq.com>
2024-10-26 22:13:06 +0800

    Update zby.py

--- zby.py
+++ zby.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-import re
 import time
 
 # 配置Chrome WebDriver
@@ -16,19 +15,19 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 url = "https://fofa.info/result?qbase64=InVkcHh5IiAmJiByZWdpb249Ikd1YW5nZG9uZyIgJiYgb3JnPSJDaGluYW5ldCI%3D"
 
 # 用于存储不重复的IP地址
-unique_links = set()
+unique_ips = set()
 
 # 访问页面
 try:
     driver.get(url)
-    time.sleep(10)  # 等待页面加载，可以根据网络情况调整等待时间
+    time.sleep(5)  # 等待页面加载，可以根据网络情况调整等待时间
 
-    # 查找包含IP地址的div元素
+    # 查找包含IP地址的span元素
     elements = driver.find_elements(By.CLASS_NAME, "hsxa-host")
     for element in elements:
-        # 提取div文本内容中的http和https链接
-        matches = re.findall(r'http[s]?://[^\s]+', element.text)
-        unique_links.update(matches)
+        ip_address = element.text.strip()
+        if ip_address:
+            unique_ips.add(ip_address)
 
 except Exception as e:
     print(f"Failed to scrape {url} due to {e}")
@@ -38,7 +37,7 @@ finally:
 
 # 将结果写入zblink.txt文件
 with open('zblink.txt', 'w') as file:
-    for link in unique_links:
-        file.write(f"{link}\n")
+    for ip in unique_ips:
+        file.write(f"{ip}\n")
 
-print(f"已抓取 {len(unique_links)} 个链接，并保存到zblink.txt文件中。")
+print(f"已抓取 {len(unique_ips)} 个IP地址，并保存到zblink.txt文件中。")
