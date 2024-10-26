d0c9e5b6d25f1c83b5bdbba2e49882466021af18
cx-bot100 <3206868632@qq.com>
2024-10-26 16:44:26 +0800

    Update zby.py

--- zby.py
+++ zby.py
@@ -5,7 +5,7 @@ import re
 # 目标URL
 url = "https://fofa.info/result?qbase64=InVkcHh5IiAmJiByZWdpb249Ikd1YW5nZG9uZyIgJiYgb3JnPSJDaGluYW5ldCI%3D"
 
-# 用于存储不重复的链接
+# 用于存储不重复的IP地址和端口
 unique_links = set()
 
 # 发起请求
@@ -19,12 +19,14 @@ try:
     # 解析网页内容
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # 查找所有class为'hsxa-host'的span标签
-    divs = soup.find_all('div', class_='hsxa-fl hsxa-meta-data-list-lv1-lf')
-    for div in divs:
-        # 使用正则表达式匹配http或https的链接
-        matches = re.findall(r'http[s]?://[^\s]+', div.get_text())
-        unique_links.update(matches)
+    # 查找所有带有data-clipboard-text属性的span标签
+    spans = soup.find_all('span', {'data-clipboard-text': True})
+    for span in spans:
+        # 提取data-clipboard-text中的内容
+        ip_port = span['data-clipboard-text']
+        # 验证IP和端口格式
+        if re.match(r'^\d+\.\d+\.\d+\.\d+:\d+$', ip_port):
+            unique_links.add(ip_port)
 
 except Exception as e:
     print(f"Failed to scrape {url} due to {e}")
@@ -34,7 +36,7 @@ valid_links = []
 
 # 验证每个链接的连接性
 for link in unique_links:
-    test_url = f"{link}/udp/status"
+    test_url = f"http://{link}/udp/239.77.0.129:5146"
     try:
         # 检查URL是否可访问
         response = requests.get(test_url, headers=headers, timeout=10)
