ddd025051fba1804885e5132eb45ea838e2ca007
cx-bot100 <3206868632@qq.com>
2024-11-06 22:40:54 +0800

    Update itv_all.py

--- itv_all.py
+++ itv_all.py
@@ -324,7 +324,7 @@ for url in urls:
 
 results = set(results)   # 去重得到唯一的URL列表
 results = sorted(results)
-with open("itv.txt", 'w', encoding='utf-8') as file:
+with open("itv_all.txt", 'w', encoding='utf-8') as file:
     for result in results:
         file.write(result + "\n")
         print(result)
